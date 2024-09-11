c8b538aa3a96459dec4ffbca429734da2158865e
chirchir <chirchir7370@gmail.com>
2024-09-11 22:57:07 +0300

    envs

--- config/test.exs
+++ config/test.exs
@@ -6,9 +6,9 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :radius, Radius.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  username: System.get_env("DB_TEST_USERNAME") || "postgres",
+  password: System.get_env("DB_TEST_PASSWORD") || "postgres",
+  hostname: System.get_env("DB_TEST_HOST") || "localhost",
   database: "radius_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
