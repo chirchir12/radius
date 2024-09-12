b43800d647fe6030d02f290aa5e88124ac2ac189
chirchir <chirchir7370@gmail.com>
2024-09-12 08:14:45 +0300

    fix getex

--- lib/radius_web/gettext.ex
+++ lib/radius_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule RadiusWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :radius
+  use Gettext.Backend, otp_app: :radius
 end
