6fbe3d60807913c50a5b942da21f5488d5394638
chirchir <chirchir7370@gmail.com>
2024-09-14 09:13:39 +0300

    unit test validate hotspot policy

--- test/radius/policies_test.exs
+++ test/radius/policies_test.exs
@@ -5,9 +5,19 @@ defmodule Radius.PoliciesTest do
 
   describe "hotspot policies" do
     @valid_attrs %{plan: Ecto.UUID.generate(), upload: 1024, download: 2048, duration: 3600}
+    @invalid_attrs %{plan: Ecto.UUID.generate(), upload: nil, download: nil, duration: nil}
 
     test "add/2 creates a hotspot policy" do
       assert {:ok, :ok} = Policies.add(:hotspot, @valid_attrs)
+    end
+
+    test "add/2 returns an error if the policy already exists" do
+      {:ok, :ok} = Policies.add(:hotspot, @valid_attrs)
+      assert {:error, :policy_exists} = Policies.add(:hotspot, @valid_attrs)
+    end
+
+    test "add/2 return invalid changeset if attrs is not valid" do
+      assert {:error, %Ecto.Changeset{valid?: false}} = Policies.add(:hotspot, @invalid_attrs)
     end
 
     test "update/2 updates a hotspot policy" do
