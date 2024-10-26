c0472bc7a2434bc88eabb4420475075c790a6340
chirchir <chirchir7370@gmail.com>
2024-10-26 10:31:04 +0300

    fix radius accounting table

--- priv/repo/migrations/20230501000007_create_radpostauth_table.exs
+++ priv/repo/migrations/20230501000007_create_radpostauth_table.exs
@@ -6,8 +6,8 @@ defmodule Radius.Repo.Migrations.CreateRadpostauthTable do
       add :username, :text, null: false
       add :pass, :text
       add :reply, :text
-      add :called_station_id, :text
-      add :calling_station_id, :text
+      add :calledstationid, :text
+      add :callingstationid, :text
       add :authdate, :utc_datetime, null: false, default: fragment("NOW()")
     end
   end
--- priv/repo/migrations/20230615123456_create_radacct_table.exs
+++ priv/repo/migrations/20230615123456_create_radacct_table.exs
@@ -3,44 +3,44 @@ defmodule Radius.Repo.Migrations.CreateRadacctTable do
 
   def change do
     create table(:radacct, primary_key: false) do
-      add :radacct_id, :bigserial, primary_key: true
-      add :acct_session_id, :text, null: false
-      add :acct_unique_id, :text, null: false
+      add :radacctid, :bigserial, primary_key: true
+      add :acctsessionid, :text, null: false
+      add :acctuniqueid, :text, null: false
       add :username, :text
       add :realm, :text
-      add :nas_ip_address, :inet, null: false
-      add :nas_port_id, :text
-      add :nas_port_type, :text
-      add :acct_start_time, :utc_datetime
-      add :acct_update_time, :utc_datetime
-      add :acct_stop_time, :utc_datetime
-      add :acct_interval, :bigint
-      add :acct_session_time, :bigint
-      add :acct_authentic, :text
-      add :connect_info_start, :text
-      add :connect_info_stop, :text
-      add :acct_input_octets, :bigint
-      add :acct_output_octets, :bigint
-      add :called_station_id, :text
-      add :calling_station_id, :text
-      add :acct_terminate_cause, :text
-      add :service_type, :text
-      add :framed_protocol, :text
-      add :framed_ip_address, :inet
+      add :nasipaddress, :inet, null: false
+      add :nasportid, :text
+      add :nasporttype, :text
+      add :acctstarttime, :utc_datetime
+      add :acctupdatetime, :utc_datetime
+      add :acctstoptime, :utc_datetime
+      add :acctinterval, :bigint
+      add :acctsessiontime, :bigint
+      add :acctauthentic, :text
+      add :connectinfo_start, :text
+      add :connectinfo_stop, :text
+      add :acctinputoctets, :bigint
+      add :acctoutputoctets, :bigint
+      add :calledstationid, :text
+      add :callingstationid, :text
+      add :acctterminatecause, :text
+      add :servicetype, :text
+      add :framedprotocol, :text
+      add :framedipaddress, :inet
     end
 
-    create unique_index(:radacct, [:acct_unique_id])
+    create unique_index(:radacct, [:acctuniqueid])
 
-    create index(:radacct, [:acct_unique_id],
+    create index(:radacct, [:acctuniqueid],
              where: "acct_stop_time IS NULL",
              name: :radacct_active_session_idx
            )
 
-    create index(:radacct, [:nas_ip_address, :acct_start_time],
+    create index(:radacct, [:nasipaddress, :acctstarttime],
              where: "acct_stop_time IS NULL",
              name: :radacct_bulk_close
            )
 
-    create index(:radacct, [:acct_start_time, :username], name: :radacct_start_user_idx)
+    create index(:radacct, [:acctstarttime, :username], name: :radacct_start_user_idx)
   end
 end
